86bc077cb7e7fc18a965f40ca0611787baa1036e
Timo Brembeck <github@timo.brembeck.email>
2026-07-05 21:07:24 +0200

    refactor: migrate builder to modern Sass compile API

--- helpers/sass.js
+++ helpers/sass.js
@@ -4,29 +4,17 @@ const fs = require("fs");
 const pathToFlagStyles = path.join(__dirname, "../src/");
 
 exports.compile = function (input, output) {
-  sass.render(
-    {
-      file: path.join(pathToFlagStyles, input + ".scss"),
-      outputStyle: "compressed",
-    },
-    function (err, result) {
-      if (!err) {
-        fs.writeFile(
-          path.join(__dirname, "../dist/", output + ".css"),
-          result.css,
-          function (err) {
-            if (!err) {
-              console.log("File written to: ", pathToFlagStyles);
-            } else {
-              console.error(err);
-              process.exitCode = 1;
-            }
-          },
-        );
-      } else {
-        console.error(err);
-        process.exitCode = 1;
-      }
-    },
-  );
+  try {
+    const result = sass.compile(path.join(pathToFlagStyles, input + ".scss"), {
+      style: "compressed",
+    });
+    fs.writeFileSync(
+      path.join(__dirname, "../dist/", output + ".css"),
+      result.css,
+    );
+    console.log("File written to: ", pathToFlagStyles);
+  } catch (err) {
+    console.error(err.message ?? err);
+    process.exitCode = 1;
+  }
 };
